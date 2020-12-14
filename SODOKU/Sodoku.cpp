e75f60ba1fcb7b6f15cbf04473f1e4d97144f256
Yehia Hosny <yehia_mamdouh95@hotmail.com>
2020-12-14 03:50:00 -0500

    Part3 half complete

--- SODOKU/Sodoku.cpp
+++ SODOKU/Sodoku.cpp
@@ -11,6 +11,8 @@
 #include "Sodoku.hpp"
 #include <fstream>
 #include <iostream>
+#include <stdlib.h>
+#include <vector>
 using namespace std;
 
 //Read Sodoku grid from a text file and save it in a 2-D Array
@@ -41,6 +43,7 @@ void Board::Print_Grid()
     }
 }
 
+//Check if the Sodoku is solved, if not, grab an empty spot and assign it with a suitable value
 bool Board::Grid_Complete()
 {
     int row;
@@ -61,11 +64,22 @@ bool Board::Grid_Complete()
     return false;
 }
 
-void Board::Print_Correction()
+//Determine the Difficulty of the grid
+void Board::Determine_Difficulty()
 {
-    cout<<"Corrected "<<m_Correction<<" Times"<<endl;
+    //Given the Examples in the project description, the number of times the code had to corret (backtrack) a number was calculated. I took the average numbers (to the nearest 10th) between difficulties and set them as constrains to determine the grid's difficulty
+    if(m_Correction < 29450)
+        m_Difficulty = "Easy";
+    else if(m_Correction >= 2945 && m_Correction < 25960)
+        m_Difficulty = "Miduim";
+    else if(m_Correction >= 25960 && m_Correction < 90900)
+        m_Difficulty = "Hard";
+    else
+        m_Difficulty = "Samurai";
+    cout<<"Corrected "<<m_Correction<<" Times."<<" Difficulty: "<<m_Difficulty<< endl;
 }
 
+//Grab an empty box (i.e. a box with 0 inside it)
 bool Board::Check_For_Zeros(int &row, int &col)
 {
     
@@ -80,6 +94,7 @@ bool Board::Check_For_Zeros(int &row, int &col)
     return true;
 }
 
+//Check if a number can be stored inside the empty box
 bool Board::Valid_Entry(int val, int row, int col)
 {
     bool Found_In_Row = false;
@@ -236,3 +251,130 @@ bool Board::Valid_Entry(int val, int row, int col)
     return !Found_In_Row && !Found_In_col && !Found_In_Box && Grid[row][col] == 0;
 }
 
+void Board::Fill_Empty_Left_Box()
+{
+    srand((unsigned) time(0));
+    vector<int> Valid_numbers_Box_1 = {1,2,3,4,5,6,7,8,9};
+    vector<int> Used_numbers_row_0 = {};
+    vector<int> Used_numbers_row_1 = {};
+    vector<int> Used_numbers_row_2 = {};
+    vector<int> Used_numbers_col_0 = {};
+    vector<int> valid_numbers_col_0 = {};
+    vector<int> temp_Used_numbers_row_0 = {};
+    vector<int> temp_Used_numbers_row_1 = {};
+    vector<int> temp_Used_numbers_row_2 = {};
+    vector<int>::iterator it;
+    int randomIndex;
+    for(int row = 0; row < 3; row++){
+        for(int col = 0; col < 3; col++)
+        {
+            randomIndex = rand() % Valid_numbers_Box_1.size();
+            Grid[row][col] = Valid_numbers_Box_1[randomIndex];
+            switch (row)
+            {
+                case 0:
+                    Used_numbers_row_0.push_back(Valid_numbers_Box_1[randomIndex]);
+                    temp_Used_numbers_row_0.push_back(Valid_numbers_Box_1[randomIndex]);
+                    break;
+                case 1:
+                    Used_numbers_row_1.push_back(Valid_numbers_Box_1[randomIndex]);
+                    temp_Used_numbers_row_1.push_back(Valid_numbers_Box_1[randomIndex]);
+                    break;
+                case 2:
+                    Used_numbers_row_2.push_back(Valid_numbers_Box_1[randomIndex]);
+                    temp_Used_numbers_row_2.push_back(Valid_numbers_Box_1[randomIndex]);
+                    break;
+                default:
+                    break;
+            }
+            
+            Valid_numbers_Box_1.erase(Valid_numbers_Box_1.begin() + randomIndex);
+            if(col == 0)
+            {
+                Used_numbers_col_0.push_back(Grid[row][col]);
+            }
+        }
+    }
+    
+    for(int row = 0; row < 3; row++){
+        for(int col = 3; col < 6; col++)
+        {
+            switch (row)
+            {
+                case 0:
+                    randomIndex = rand() % Used_numbers_row_2.size();
+                    Grid[row][col] = Used_numbers_row_2[randomIndex];
+                    Used_numbers_row_2.erase(Used_numbers_row_2.begin() + randomIndex);
+                    break;
+                case 1:
+                    randomIndex = rand() % Used_numbers_row_0.size();
+                    Grid[row][col] = Used_numbers_row_0[randomIndex];
+                    Used_numbers_row_0.erase(Used_numbers_row_0.begin() + randomIndex);
+                    break;
+                case 2:
+                    randomIndex = rand() % Used_numbers_row_1.size();
+                    Grid[row][col] = Used_numbers_row_1[randomIndex];
+                    Used_numbers_row_1.erase(Used_numbers_row_1.begin() + randomIndex);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    
+    for(int row = 0; row < 3; row++){
+        for(int col = 6; col < 9; col++)
+        {
+            switch (row)
+            {
+                case 0:
+                    randomIndex = rand() % temp_Used_numbers_row_1.size();
+                    Grid[row][col] = temp_Used_numbers_row_1[randomIndex];
+                    temp_Used_numbers_row_1.erase(temp_Used_numbers_row_1.begin() + randomIndex);
+                    break;
+                case 1:
+                    randomIndex = rand() % temp_Used_numbers_row_2.size();
+                    Grid[row][col] = temp_Used_numbers_row_2[randomIndex];
+                    temp_Used_numbers_row_2.erase(temp_Used_numbers_row_2.begin() + randomIndex);
+                    break;
+                case 2:
+                    randomIndex = rand() % temp_Used_numbers_row_0.size();
+                    Grid[row][col] = temp_Used_numbers_row_0[randomIndex];
+                    temp_Used_numbers_row_0.erase(temp_Used_numbers_row_0.begin() + randomIndex);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    
+    for(int i = 1; i<10; i++)
+    {
+        bool found = false;
+        for(int j = 0; j < Used_numbers_col_0.size(); j++)
+        {
+            if(i == Used_numbers_col_0[j])
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            valid_numbers_col_0.push_back(i);
+    }
+    
+    for(int row = 3; row<9; row++)
+    {
+        randomIndex = rand() % valid_numbers_col_0.size();
+        Grid[row][0] = valid_numbers_col_0[randomIndex];
+         valid_numbers_col_0.erase(valid_numbers_col_0.begin() + randomIndex);
+    }
+    
+    Grid_Complete();
+
+}
+
+void Board::Fill_Empty_Grid2()
+{
+    
+}
--- SODOKU/Sodoku.hpp
+++ SODOKU/Sodoku.hpp
@@ -23,13 +23,24 @@ public:
     bool Check_For_Zeros(int &row, int& col);
     bool Grid_Complete();
     bool Valid_Entry(int val, int row, int col);
-    void Print_Correction();
+    void Determine_Difficulty();
+    void Fill_Empty_Left_Box();
+    void Fill_Empty_Grid2();
 private:
     
-    int Grid[9][9]{};
+    int Grid[9][9]{0};
     int m_Correction;
+    string m_Difficulty;
     
 };
 
+/*class Puzzle : public Board
+{
+public:
+    void Fill_Empty_Grid();
+private:
+    int Grid[9][9]{};
+};
+*/
 #endif /* Sodoku_hpp */
 
